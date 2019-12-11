f026e83b754aeb41af40f9ff716c8eec3e7c7ca8
Pouya Abbassi <abbassi.pouya@gmail.com>
2019-12-11 21:51:52 +0330

    remove duplication for contact email

--- pelicanconf.py
+++ pelicanconf.py
@@ -9,6 +9,7 @@ AUTHOR = 'Pouya Abbassi'
 SITENAME = 'PouyaCode'
 DESCRIPTION = 'A place for my thoughts.'
 SITEURL = 'http://localhost:8000'
+FEEDBACK = 'mailto:&#112;&#99;&#111;&#100;&#101;&#64;&#112;&#114;&#111;&#116;&#111;&#110;&#109;&#97;&#105;&#108;&#46;&#99;&#111;&#109;'
 
 PATH = 'content'
 
@@ -36,7 +37,7 @@ SOCIAL = (
         ('Gitlab', 'https://gitlab.com/pouya-abbassi/', 'fab fa-gitlab'),
         ('Devheroes', 'https://devheroes.club/?u=pouya-abbassi', 'fas fa-hands-helping'),
         ('Goodreads', 'https://www.goodreads.com/pouyacode', 'fab fa-goodreads'),
-        ('Email', 'mailto:&#112;&#99;&#111;&#100;&#101;&#64;&#112;&#114;&#111;&#116;&#111;&#110;&#109;&#97;&#105;&#108;&#46;&#99;&#111;&#109;', 'fas fa-at'),
+        ('Email', FEEDBACK, 'fas fa-at'),
         ('Atom feed', 'feeds/all.atom.xml', 'fas fa-rss'),
         )
 
