46ec7283153c1e0f165dc908861722f6c0460633
Pouya Abbassi <abbassi.pouya@gmail.com>
2019-12-12 00:43:54 +0330

    Cache handling

--- createfiles.py
+++ createfiles.py
@@ -4,8 +4,9 @@ def create():
         robots.close()
 
     with open('output/.htaccess', 'w') as htaccess:
-        htaccess.write('#Comment | Force all URLs to https WITHOUT www\nRewriteCond %{SERVER_PORT} 80\nRewriteRule ^(.*)$ https://pouyacode.net/$1 [R,L]\n\n#Comment | Force all URLs to https WITH www\nRewriteCond %{SERVER_PORT} 80\nRewriteRule ^(.*)$ https://pouyacode.net/$1 [R,L]')
+        htaccess.write('<ifModule mod_headers.c>\n  # 1 Day for html, xml and txt\n  <filesMatch ".(html|htm|shtml|xml|txt)$">\n  Header set Cache-Control "max-age=86400, public, must-revalidate"\n  </filesMatch>\n\n  # 1 Month for most static assets\n  <filesMatch ".(css|js|svg|jpg|jpeg|png|gif|ico)$">\n  Header set Cache-Control "max-age=2592000, public"\n  </filesMatch>\n\n  # 1 Year for fonts\n  <filesMatch ".(woff|woff2|ttf|otf|eot)$">\n  Header set Cache-Control "max-age=31104000, public"\n  </filesMatch>\n</ifModule>\n\nRewriteEngine On\nRewriteCond %{HTTPS} off [OR]\nRewriteCond %{HTTP_HOST} ^www\. [NC]\nRewriteCond %{HTTP_HOST} ^(?:www\.)?(.+)$ [NC]\nRewriteRule ^ https://%1%{REQUEST_URI} [L,NE,R=301]')
         htaccess.close()
+
 
 if __name__ == '__main__':
     create()
